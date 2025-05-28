4dc61816283971777aa333d22055c7458e7b3ce5
Santhosh Cheemala <146196375+SanthoshCheemala@users.noreply.github.com>
2025-05-28 19:17:45 +0530

    compressFun

--- hash/sha256.go
+++ hash/sha256.go
@@ -1,6 +1,7 @@
 package hash
 
 import (
+	"bytes"
 	"encoding/binary"
 	"fmt"
 
@@ -10,6 +11,25 @@ import (
 type SHA256State struct{
 	State []byte
 }
+var k = [64]uint32{
+	0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
+	0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+	0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
+	0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+	0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
+	0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+	0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
+	0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+	0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
+	0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+	0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
+	0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+	0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
+	0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+	0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+	0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
+}
+
 
 func NewSHA256State() *SHA256State{
 
@@ -40,7 +60,7 @@ func Sha256(msg string, iv []byte) *SHA256State{
 func (s *SHA256State) ProcessBlock(block []byte){
 	tmp := make([]byte,len(s.State))
 	copy(tmp,s.State)
-	s.compressFun(block,tmp)
+	s.State = compressFun(block,tmp)
 	s.State = addMod32(s.State,tmp)
 }
 
@@ -62,7 +82,36 @@ func  addMod32(b []byte, tmp []byte) []byte {
 }
 
 
-func (s *SHA256State) compressFun(b []byte, tmp []byte) {
+func compressFun(b []byte, h []byte) []byte {
+	tmp := make([]uint32,len(h)*8/32)
+	w := make([]uint32,64)
+	for i := 0; i < 8; i++{
+		tmp[i] = binary.BigEndian.Uint32(h[i*4:i*4+4])
+	}
+	for i := 0; i < 16; i ++{
+		w[i] = binary.BigEndian.Uint32(b[i*4:i*4+4])
+		tmp = roundFunc(w[i],tmp,k[i])	
+	}
+	for i := 16; i < len(w); i++{
+		w[i] = w[i-16] + sigma0(w[i-15]) + w[i-7] + sigma1(w[i-2])
+		tmp = roundFunc(w[i],tmp,k[i])
+	}
+	for i := 0; i < 8; i++{
+		binary.BigEndian.PutUint32(h[i*4:i*4+4],tmp[i])
+	}
+	return h
+}
+
+func sigma1(u uint32) uint32 {
 	panic("unimplemented")
 }
+
+func sigma0(u uint32) uint32 {
+	panic("unimplemented")
+}
+
+func roundFunc(u1 uint32, tmp []uint32, u2 uint32) []uint32 {
+	panic("unimplemented")
+}
+
 
