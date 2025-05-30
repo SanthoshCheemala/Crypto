ea03490485b52c4169cd028a3fd10c7a83dffd10
Santhosh Cheemala <146196375+SanthoshCheemala@users.noreply.github.com>
2025-05-30 18:51:26 +0530

    sha256 testing completed

--- hash/sha256_test.go
+++ hash/sha256_test.go
@@ -1,1 +1,23 @@
 package hash
+
+import (
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+
+func TestSha256(t *testing.T){
+	var (
+		msg = "this is message.Please hash this value"
+		hexhashmsg = "da64e014912667baead7f1d5dc4262b28bbf547e00cb3b5a9171f43e8bf8f006"
+	)
+
+	h := NewSHA256State()
+	h.Sha256(msg)
+	fmt.Println(h.State)
+	fmt.Println(h.Sum(),hexhashmsg)
+	if  !reflect.DeepEqual(h.Sum(),hexhashmsg){
+		t.Fatalf("hashing failed")
+	}
+}
