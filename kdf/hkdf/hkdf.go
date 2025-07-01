cacb831bfe8f2d7e253782fee94c68e4e05b7bf7
Santhosh Cheemala <146196375+SanthoshCheemala@users.noreply.github.com>
2025-07-01 18:49:08 +0530

    testing hkdf

--- kdf/hkdf/hkdf.go
+++ kdf/hkdf/hkdf.go
@@ -1,17 +1,19 @@
 package hkdf
 
 import (
+	"crypto/sha256"
 	"errors"
+
 	"github.com/SanthoshCheemala/Crypto/kdf/hmac"
 )
 
-
+const hashLen = sha256.Size
 
 func extract(secret, salt []byte) []byte {
 	if salt == nil {
-		salt = make([]byte,32)
+		salt = make([]byte, hashLen)
 	}
-	return hmac.HMAC_Sign(salt,secret) 
+	return hmac.HMAC_Sign(salt, secret)
 }
 
 
@@ -20,21 +22,24 @@ func New(secret, salt, info []byte,length int) ([]byte,error) {
 	return expand(prk,info,length)
 }
 
-func expand(prk []byte, info []byte,length int) ([]byte,error) {
+func expand(prk []byte, info []byte, length int) ([]byte, error) {
 	
-	if length > 255*32 {
-		return nil,errors.New("hkdf: output length is too large")
+	if length > 255*hashLen {
+		return nil, errors.New("hkdf: output length is too large")
 	}
 
 	var okm []byte
-	var previous []byte
+	var t []byte
 	counter := byte(1)
 
 	for len(okm) < length {
-		input := append(append(previous, info...),counter)
-		mac := hmac.HMAC_Sign(prk,input)
-		okm = append(okm, mac...)
-		previous = mac
+		bufCap := len(t) + len(info) + 1
+		input := make([]byte, 0, bufCap)
+		input = append(input, t...)
+		input = append(input, info...)
+		input = append(input, counter)
+		t = hmac.HMAC_Sign(prk, input)
+		okm = append(okm, t...)
 		counter++
 	}
 	return okm[:length], nil
--- kdf/hkdf/hkdf_test.go
+++ kdf/hkdf/hkdf_test.go
@@ -1,1 +1,104 @@
 package hkdf
+
+import (
+	"encoding/hex"
+	"fmt"
+	"reflect"
+	"testing"
+)
+
+func decodeHex(s string) []byte {
+	b,_ := hex.DecodeString(s)
+	return b
+}
+
+func TestHKDF(t *testing.T) {
+	ikm := decodeHex("0b0b0b0b0b0b0b0b0b0b0b")
+	salt := decodeHex("000102030405060708090a0b0c")
+	info := decodeHex("f0f1f2f3f4f5f6f7f8f9")
+	length := 16
+
+	okmExpected := decodeHex("d307356a0d2609a15cfaf24245b6c84b")
+	okm,err := New(ikm,salt,info,length)
+	if err != nil {
+		t.Errorf("HKDF failed! %v",err)
+	}
+	if  !reflect.DeepEqual(okm,okmExpected) {
+		fmt.Println(hex.EncodeToString(okm))
+		t.Errorf("Hkdf failed! okmExpected is not equal to okm")
+	}
+}
+
+func TestHKDF_EmptySalt(t *testing.T) {
+	ikm := decodeHex("0b0b0b0b0b0b0b0b0b0b0b")
+	salt := decodeHex("")
+	info := decodeHex("f0f1f2f3f4f5f6f7f8f9")
+	length := 16
+
+	okmExpected := decodeHex("582d277ad9fdf7ac8361301b5e3edd62")
+	okm,err := New(ikm,salt,info,length)
+	if err != nil {
+		t.Errorf("HKDF failed! %v",err)
+	}
+	if  !reflect.DeepEqual(okm,okmExpected) {
+		fmt.Println(hex.EncodeToString(okm))
+		t.Errorf("Hkdf failed! okmExpected is not equal to okm")
+	}
+}
+
+func TestHKDF_TooLong(t *testing.T) {
+	ikm := decodeHex("0b0b0b0b0b0b0b0b0b0b0b")
+	info := decodeHex("f0f1f2f3f4f5f6f7f8f9")
+	length := 255*32 + 1
+
+	_,err := New(ikm,nil,info,length)
+	if err == nil {
+		t.Errorf("expected error due to excessive length, but got nil")
+	}
+}
+
+
+func BenchmarkHKDF_32Bytes(b *testing.B) {
+	ikm := decodeHex("0b0b0b0b0b0b0b0b0b0b0b")
+	salt := decodeHex("000102030405060708090a0b0c")
+	info := decodeHex("f0f1f2f3f4f5f6f7f8f9")
+	length := 32
+
+	for i := 0; i < b.N; i++ {
+		New(ikm,salt,info,length)
+	}
+}
+
+
+func BenchmarkHKDF_512Bytes(b *testing.B) {
+	ikm := decodeHex("0b0b0b0b0b0b0b0b0b0b0b")
+	salt := decodeHex("000102030405060708090a0b0c")
+	info := decodeHex("f0f1f2f3f4f5f6f7f8f9")
+	length := 512
+
+	for i := 0; i < b.N; i++ {
+		New(ikm,salt,info,length)
+	}
+}
+
+func BenchmarkHKDF_4096Bytes(b *testing.B) {
+	ikm := decodeHex("0b0b0b0b0b0b0b0b0b0b0b")
+	salt := decodeHex("000102030405060708090a0b0c")
+	info := decodeHex("f0f1f2f3f4f5f6f7f8f9")
+	length := 4096
+
+	for i := 0; i < b.N; i++ {
+		New(ikm,salt,info,length)
+	}
+}
+
+func BenchmarkHKDF_Bytes(b *testing.B) {
+	ikm := decodeHex("0b0b0b0b0b0b0b0b0b0b0b")
+	salt := decodeHex("000102030405060708090a0b0c")
+	info := decodeHex("f0f1f2f3f4f5f6f7f8f9")
+	length := 8160
+
+	for i := 0; i < b.N; i++ {
+		New(ikm,salt,info,length)
+	}
+}
